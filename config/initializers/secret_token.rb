40c648ac56baa79fa265469c81b5dc81336ce1ac
Ruben Quintero <rfquintero@gmail.com>
2013-06-03 15:58:10 -0500

    Initial commit
    - ruben

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BradoSp::Application.config.secret_token = '633c78194c4763f3ae9d529ae0f1f27296a146f20bba8f338513c4813e0321226adab9ea2cc670b24467648ccbd5530b72e5cf7fc2b12a2aee8527aefd4cf4ec'
